782409661e25d7364e09941deee65fbbb7e3fee9
frenkiart <frenki_art@mail.ru>
2021-03-23 03:03:20 +0300

    Test

--- src/index.js
+++ src/index.js
@@ -5,9 +5,18 @@ import {Header} from './components/header/Header';
 import {Toolbar} from './components/toolbar/Toolbar';
 import {Formula} from './components/formula/Formula';
 import {Table} from './components/table/Table';
+// import {Emitter} from './core/Emitter';
 
 const excel = new Excel('#app', {
   components: [Header, Toolbar, Formula, Table],
 });
 
 excel.render();
+
+/*
+* sdf example for test
+const emitter = new Emitter();
+emitter.subscribe('vladilen', (data) => console.log('Sub: ', data));
+
+emitter.emit('vladilen', 43);
+ */
